4d3d8ea1972c2f66a25d146691c39c3c55da706a
Adam Gądziak <agadziak@opera.com>
2016-09-21 18:07:46 +0200

    add promises

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const API_KEY = '406fa65fd7e94f9fb7849e3a9c784a52';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
@@ -5,7 +7,10 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather() {
 	const url = `${ROOT_URL}&q=${city},pl`;
+	const request = axios.get(url);
+
 	return {
-		type: FETCH_WEATHER
+		type: FETCH_WEATHER,
+		payload: request
 	};
 }
