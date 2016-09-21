e405d0bbf61bade60e1d1cab70b2ca7bc6d57e36
Adam Gądziak <agadziak@opera.com>
2016-09-21 18:04:32 +0200

    init

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+const API_KEY = '406fa65fd7e94f9fb7849e3a9c784a52';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather() {
+	const url = `${ROOT_URL}&q=${city},pl`;
+	return {
+		type: FETCH_WEATHER
+	};
+}
